46277177bf20dbe5560a87bae6c6ec807937dc57
Anup-Nayak <gpnayak1930@gmail.com>
2024-03-05 17:25:04 +0530

    pdf text done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import assemblyai as aai
+import PyPDF2
 
 def transcribeAV(av, file_name):
 
@@ -18,6 +19,22 @@ def transcribeAV(av, file_name):
     output_file.close()
     # print(transcript.text)
 
+def extractPdf(file_name):
+    with open(file_name,'rb') as pdf:
+        reader = PyPDF2.PdfReader(pdf,strict = False)
+        pdf_text = []
+
+        for page in reader.pages:
+            content = page.extract_text()
+            pdf_text.append(content)
+               
+    with open('transcript.txt', "w") as output_file:
+        for text in pdf_text:
+            output_file.write(text)
+
+    print(f"Transcript written to transcript.txt")
+    output_file.close()
+
 if __name__ == "__main__":
     file_type = int(input("1 for video, 2 for audio, 3 for pdf: "))
     file_name = input("Enter the file name you want to transcribe: ")
@@ -26,4 +43,4 @@ if __name__ == "__main__":
     elif file_type == 2:
         transcribeAV("audio",file_name)
     else:
-        print("hello")
+        extractPdf(file_name)
